43bd90eae019308a890023c4fe0363c8ec929b22
sola_c_plus <Akinosora.9710@gmail.com>
2025-06-14 23:38:58 +0900

    Update arduino_show_RGB_LED.ino

--- arduino_show_RGB_LED.ino
+++ arduino_show_RGB_LED.ino
@@ -9,9 +9,9 @@ void setup() {
    FastLED.addLeds<WS2812B, controlPin, GRB>(leds, numberOfLEDs);
    FastLED.show(); 
 
-   leds[0].r = 255;//leds[]のカッコ内は色を変更するLEDの番号を入力
-   leds[0].g = 255;//
-   leds[0].b = 255;
+   leds[0].r = 255;//赤色leds[]のカッコ内は色を変更するLEDの番号を入力
+   leds[0].g = 255;//緑色
+   leds[0].b = 255;//青色
 
    FastLED.show();//変更時　最後に記載する
 }
